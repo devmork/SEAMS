be481b081232e0d52bbe2a8288ac425fbc894552
Den Enoy <132737377+devmork@users.noreply.github.com>
2025-04-06 19:36:49 +0800

    refactor: improved the ui of "Events" form

--- AttendanceManagementSystem/Forms/Events/UserControl_Events.cs
+++ AttendanceManagementSystem/Forms/Events/UserControl_Events.cs
@@ -1,21 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
+using System.Data;
 using System.Text;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using DevExpress.XtraEditors;
 
-namespace AttendanceManagementSystem.User_Controls
+namespace AttendanceManagementSystem.Forms.Events
 {
-    public partial class UserControl_Events : DevExpress.XtraEditors.XtraUserControl
-    {
+	public partial class UserControl_Events: DevExpress.XtraEditors.XtraUserControl
+	{
         public UserControl_Events()
-        {
+		{
             InitializeComponent();
-        }
-    }
+		}
+	}
 }
