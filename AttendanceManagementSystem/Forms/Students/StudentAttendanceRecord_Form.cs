0d1ed639cae69c348fbb36eaefc35ffdb7de2e23
Den Enoy <132737377+devmork@users.noreply.github.com>
2025-04-11 20:24:05 +0800

    feat: add "Student Attendance Record" form

---
+++ AttendanceManagementSystem/Forms/Students/StudentAttendanceRecord_Form.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace AttendanceManagementSystem.Forms.Students
+{
+	public partial class StudentAttendanceRecord_Form: DevExpress.XtraEditors.XtraForm
+	{
+        public StudentAttendanceRecord_Form()
+		{
+            InitializeComponent();
+		}
+	}
+}
