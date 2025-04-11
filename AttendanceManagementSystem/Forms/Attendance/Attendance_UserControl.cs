8e1b094c0b8b5a55c36f03bda73a1634b7311ee2
Den Enoy <132737377+devmork@users.noreply.github.com>
2025-04-11 18:31:50 +0800

    refactor: created new user control for  "Attendance" form

---
+++ AttendanceManagementSystem/Forms/Attendance/Attendance_UserControl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace AttendanceManagementSystem.Forms.Attendance
+{
+	public partial class Attendance_UserControl: DevExpress.XtraEditors.XtraUserControl
+	{
+        public Attendance_UserControl()
+		{
+            InitializeComponent();
+		}
+	}
+}
