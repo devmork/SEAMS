8907c54af56548a8ea37ca47f74e07cfe997cd3f
Den Enoy <132737377+devmork@users.noreply.github.com>
2025-04-18 12:18:54 +0800

    Add a base class Person

---
+++ AttendanceManagementSystem/Models/Base/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttendanceManagementSystem.Models.Base
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+
+        public Person(string firstName, string middleName, string lastName)
+        {
+            FirstName = firstName;
+            MiddleName = middleName;
+            LastName = lastName;
+        }
+    }
+}
