ac26ff3f84a9b5bd04c1028bfc2c32b8d6848ccf
Den Enoy <132737377+devmork@users.noreply.github.com>
2025-04-21 13:15:41 +0800

    refactor: improve code logic and process

--- AttendanceManagementSystem/Forms/Students/AddStudent_Form.Designer.cs
+++ AttendanceManagementSystem/Forms/Students/AddStudent_Form.Designer.cs
@@ -77,10 +77,9 @@
             this.lc_AddStudent.Controls.Add(this.panel_SaveStudentData);
             this.lc_AddStudent.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lc_AddStudent.Location = new System.Drawing.Point(0, 0);
-            this.lc_AddStudent.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.lc_AddStudent.Name = "lc_AddStudent";
             this.lc_AddStudent.Root = this.Root;
-            this.lc_AddStudent.Size = new System.Drawing.Size(758, 601);
+            this.lc_AddStudent.Size = new System.Drawing.Size(663, 457);
             this.lc_AddStudent.TabIndex = 0;
             this.lc_AddStudent.Text = "layoutControl1";
             // 
@@ -100,139 +99,151 @@
             this.gc_StudentData.Controls.Add(this.txt_LastName);
             this.gc_StudentData.Controls.Add(this.txt_MiddleName);
             this.gc_StudentData.Controls.Add(this.txt_FirstName);
-            this.gc_StudentData.Location = new System.Drawing.Point(18, 21);
-            this.gc_StudentData.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.gc_StudentData.Location = new System.Drawing.Point(14, 12);
             this.gc_StudentData.Name = "gc_StudentData";
-            this.gc_StudentData.Size = new System.Drawing.Size(388, 559);
+            this.gc_StudentData.Size = new System.Drawing.Size(341, 433);
             this.gc_StudentData.TabIndex = 6;
             this.gc_StudentData.Text = "Student Data";
             // 
             // label_EmailAddress
             // 
-            this.label_EmailAddress.Location = new System.Drawing.Point(18, 462);
-            this.label_EmailAddress.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.label_EmailAddress.Location = new System.Drawing.Point(16, 351);
             this.label_EmailAddress.Name = "label_EmailAddress";
-            this.label_EmailAddress.Size = new System.Drawing.Size(94, 25);
+            this.label_EmailAddress.Size = new System.Drawing.Size(77, 19);
             this.label_EmailAddress.TabIndex = 14;
             this.label_EmailAddress.Text = "Email Address";
             // 
             // label_YearLevel
             // 
-            this.label_YearLevel.Location = new System.Drawing.Point(185, 392);
-            this.label_YearLevel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.label_YearLevel.Location = new System.Drawing.Point(162, 298);
             this.label_YearLevel.Name = "label_YearLevel";
-            this.label_YearLevel.Size = new System.Drawing.Size(69, 25);
+            this.label_YearLevel.Size = new System.Drawing.Size(56, 19);
             this.label_YearLevel.TabIndex = 13;
             this.label_YearLevel.Text = "Year Level";
             // 
             // label_Course
             // 
-            this.label_Course.Location = new System.Drawing.Point(17, 392);
-            this.label_Course.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.label_Course.Location = new System.Drawing.Point(15, 298);
             this.label_Course.Name = "label_Course";
-            this.label_Course.Size = new System.Drawing.Size(50, 25);
+            this.label_Course.Size = new System.Drawing.Size(39, 19);
             this.label_Course.TabIndex = 12;
             this.label_Course.Text = "Course";
             // 
             // label_SchoolStudentId
             // 
-            this.label_SchoolStudentId.Location = new System.Drawing.Point(18, 313);
-            this.label_SchoolStudentId.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.label_SchoolStudentId.Location = new System.Drawing.Point(16, 238);
             this.label_SchoolStudentId.Name = "label_SchoolStudentId";
-            this.label_SchoolStudentId.Size = new System.Drawing.Size(63, 25);
+            this.label_SchoolStudentId.Size = new System.Drawing.Size(51, 19);
             this.label_SchoolStudentId.TabIndex = 11;
             this.label_SchoolStudentId.Text = "School ID";
             // 
             // label_LastName
             // 
-            this.label_LastName.Location = new System.Drawing.Point(17, 228);
-            this.label_LastName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.label_LastName.Location = new System.Drawing.Point(15, 173);
             this.label_LastName.Name = "label_LastName";
-            this.label_LastName.Size = new System.Drawing.Size(72, 25);
+            this.label_LastName.Size = new System.Drawing.Size(58, 19);
             this.label_LastName.TabIndex = 10;
             this.label_LastName.Text = "Last Name";
             // 
             // label_MiddleName
             // 
-            this.label_MiddleName.Location = new System.Drawing.Point(18, 138);
-            this.label_MiddleName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.label_MiddleName.Location = new System.Drawing.Point(16, 105);
             this.label_MiddleName.Name = "label_MiddleName";
-            this.label_MiddleName.Size = new System.Drawing.Size(90, 25);
+            this.label_MiddleName.Size = new System.Drawing.Size(72, 19);
             this.label_MiddleName.TabIndex = 9;
             this.label_MiddleName.Text = "Middle Name";
             // 
             // label_FirstName
             // 
-            this.label_FirstName.Location = new System.Drawing.Point(17, 50);
-            this.label_FirstName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.label_FirstName.Location = new System.Drawing.Point(15, 38);
             this.label_FirstName.Name = "label_FirstName";
-            this.label_FirstName.Size = new System.Drawing.Size(72, 25);
+            this.label_FirstName.Size = new System.Drawing.Size(59, 19);
             this.label_FirstName.TabIndex = 8;
             this.label_FirstName.Text = "First Name";
             // 
             // se_YearLevel
             // 
             this.se_YearLevel.EditValue = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.se_YearLevel.Location = new System.Drawing.Point(162, 317);
+            this.se_YearLevel.Name = "se_YearLevel";
+            this.se_YearLevel.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.se_YearLevel.Properties.Increment = new decimal(new int[] {
             0,
             0,
             0,
             0});
-            this.se_YearLevel.Location = new System.Drawing.Point(185, 417);
-            this.se_YearLevel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.se_YearLevel.Name = "se_YearLevel";
-            this.se_YearLevel.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
-            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
-            this.se_YearLevel.Size = new System.Drawing.Size(177, 44);
+            this.se_YearLevel.Properties.IsFloatValue = false;
+            this.se_YearLevel.Properties.MaskSettings.Set("mask", "N00");
+            this.se_YearLevel.Properties.MaxValue = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.se_YearLevel.Properties.MinValue = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.se_YearLevel.Size = new System.Drawing.Size(155, 34);
             this.se_YearLevel.TabIndex = 7;
             // 
             // cbe_Course
             // 
-            this.cbe_Course.Location = new System.Drawing.Point(18, 417);
-            this.cbe_Course.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cbe_Course.Location = new System.Drawing.Point(16, 317);
             this.cbe_Course.Name = "cbe_Course";
             this.cbe_Course.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
             new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
-            this.cbe_Course.Size = new System.Drawing.Size(160, 44);
+            this.cbe_Course.Properties.Items.AddRange(new object[] {
+            "BSN",
+            "BSMT",
+            "BSIT",
+            "BSBA",
+            "BSHM",
+            "BSED",
+            "BSPT",
+            "BS-PHARMA",
+            "BSRT"});
+            this.cbe_Course.Size = new System.Drawing.Size(140, 34);
             this.cbe_Course.TabIndex = 6;
             // 
             // txt_EmailAddress
             // 
-            this.txt_EmailAddress.Location = new System.Drawing.Point(17, 493);
-            this.txt_EmailAddress.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_EmailAddress.Location = new System.Drawing.Point(15, 375);
             this.txt_EmailAddress.Name = "txt_EmailAddress";
-            this.txt_EmailAddress.Size = new System.Drawing.Size(345, 44);
+            this.txt_EmailAddress.Size = new System.Drawing.Size(302, 34);
             this.txt_EmailAddress.TabIndex = 4;
             // 
             // txt_SchoolStudentId
             // 
-            this.txt_SchoolStudentId.Location = new System.Drawing.Point(18, 339);
-            this.txt_SchoolStudentId.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_SchoolStudentId.Location = new System.Drawing.Point(16, 258);
             this.txt_SchoolStudentId.Name = "txt_SchoolStudentId";
-            this.txt_SchoolStudentId.Size = new System.Drawing.Size(344, 44);
+            this.txt_SchoolStudentId.Size = new System.Drawing.Size(301, 34);
             this.txt_SchoolStudentId.TabIndex = 3;
             // 
             // txt_LastName
             // 
-            this.txt_LastName.Location = new System.Drawing.Point(18, 261);
-            this.txt_LastName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_LastName.Location = new System.Drawing.Point(16, 198);
             this.txt_LastName.Name = "txt_LastName";
-            this.txt_LastName.Size = new System.Drawing.Size(344, 44);
+            this.txt_LastName.Size = new System.Drawing.Size(301, 34);
             this.txt_LastName.TabIndex = 2;
             // 
             // txt_MiddleName
             // 
-            this.txt_MiddleName.Location = new System.Drawing.Point(18, 171);
-            this.txt_MiddleName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_MiddleName.Location = new System.Drawing.Point(16, 130);
             this.txt_MiddleName.Name = "txt_MiddleName";
-            this.txt_MiddleName.Size = new System.Drawing.Size(344, 44);
+            this.txt_MiddleName.Size = new System.Drawing.Size(301, 34);
             this.txt_MiddleName.TabIndex = 1;
             // 
             // txt_FirstName
             // 
-            this.txt_FirstName.Location = new System.Drawing.Point(17, 83);
-            this.txt_FirstName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_FirstName.Location = new System.Drawing.Point(15, 63);
             this.txt_FirstName.Name = "txt_FirstName";
-            this.txt_FirstName.Size = new System.Drawing.Size(345, 44);
+            this.txt_FirstName.Size = new System.Drawing.Size(302, 34);
             this.txt_FirstName.TabIndex = 0;
             // 
             // panel_SaveStudentData
@@ -241,37 +252,33 @@
             this.panel_SaveStudentData.Controls.Add(this.btn_Save);
             this.panel_SaveStudentData.Controls.Add(this.btn_Generate);
             this.panel_SaveStudentData.Controls.Add(this.pe_QRCode);
-            this.panel_SaveStudentData.Location = new System.Drawing.Point(412, 21);
-            this.panel_SaveStudentData.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel_SaveStudentData.Location = new System.Drawing.Point(361, 12);
             this.panel_SaveStudentData.Name = "panel_SaveStudentData";
-            this.panel_SaveStudentData.Size = new System.Drawing.Size(328, 559);
+            this.panel_SaveStudentData.Size = new System.Drawing.Size(288, 433);
             this.panel_SaveStudentData.TabIndex = 5;
             // 
             // btn_Cancel
             // 
-            this.btn_Cancel.Location = new System.Drawing.Point(6, 493);
-            this.btn_Cancel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_Cancel.Location = new System.Drawing.Point(5, 375);
             this.btn_Cancel.Name = "btn_Cancel";
-            this.btn_Cancel.Size = new System.Drawing.Size(315, 45);
+            this.btn_Cancel.Size = new System.Drawing.Size(276, 34);
             this.btn_Cancel.TabIndex = 3;
             this.btn_Cancel.Text = "CANCEL";
             // 
             // btn_Save
             // 
-            this.btn_Save.Location = new System.Drawing.Point(6, 442);
-            this.btn_Save.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_Save.Location = new System.Drawing.Point(5, 336);
             this.btn_Save.Name = "btn_Save";
-            this.btn_Save.Size = new System.Drawing.Size(315, 45);
+            this.btn_Save.Size = new System.Drawing.Size(276, 34);
             this.btn_Save.TabIndex = 2;
             this.btn_Save.Text = "SAVE";
             this.btn_Save.Click += new System.EventHandler(this.btn_Save_Click);
             // 
             // btn_Generate
             // 
-            this.btn_Generate.Location = new System.Drawing.Point(6, 313);
-            this.btn_Generate.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_Generate.Location = new System.Drawing.Point(5, 238);
             this.btn_Generate.Name = "btn_Generate";
-            this.btn_Generate.Size = new System.Drawing.Size(315, 45);
+            this.btn_Generate.Size = new System.Drawing.Size(276, 34);
             this.btn_Generate.TabIndex = 1;
             this.btn_Generate.Text = "GENERATE";
             this.btn_Generate.Click += new System.EventHandler(this.btn_Generate_Click);
@@ -280,10 +287,9 @@
             // 
             this.pe_QRCode.Dock = System.Windows.Forms.DockStyle.Top;
             this.pe_QRCode.Location = new System.Drawing.Point(2, 2);
-            this.pe_QRCode.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pe_QRCode.Name = "pe_QRCode";
             this.pe_QRCode.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
-            this.pe_QRCode.Size = new System.Drawing.Size(324, 303);
+            this.pe_QRCode.Size = new System.Drawing.Size(284, 230);
             this.pe_QRCode.TabIndex = 0;
             // 
             // Root
@@ -294,15 +300,15 @@
             this.lci_SaveStudentData,
             this.lci_StudentData});
             this.Root.Name = "Root";
-            this.Root.Size = new System.Drawing.Size(758, 601);
+            this.Root.Size = new System.Drawing.Size(663, 457);
             this.Root.TextVisible = false;
             // 
             // lci_SaveStudentData
             // 
             this.lci_SaveStudentData.Control = this.panel_SaveStudentData;
-            this.lci_SaveStudentData.Location = new System.Drawing.Point(394, 0);
+            this.lci_SaveStudentData.Location = new System.Drawing.Point(347, 0);
             this.lci_SaveStudentData.Name = "lci_SaveStudentData";
-            this.lci_SaveStudentData.Size = new System.Drawing.Size(334, 567);
+            this.lci_SaveStudentData.Size = new System.Drawing.Size(294, 437);
             this.lci_SaveStudentData.TextSize = new System.Drawing.Size(0, 0);
             this.lci_SaveStudentData.TextVisible = false;
             // 
@@ -311,17 +317,16 @@
             this.lci_StudentData.Control = this.gc_StudentData;
             this.lci_StudentData.Location = new System.Drawing.Point(0, 0);
             this.lci_StudentData.Name = "lci_StudentData";
-            this.lci_StudentData.Size = new System.Drawing.Size(394, 567);
+            this.lci_StudentData.Size = new System.Drawing.Size(347, 437);
             this.lci_StudentData.TextSize = new System.Drawing.Size(0, 0);
             this.lci_StudentData.TextVisible = false;
             // 
             // AddStudent_Form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(758, 601);
+            this.ClientSize = new System.Drawing.Size(663, 457);
             this.Controls.Add(this.lc_AddStudent);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "AddStudent_Form";
             this.Text = "AddStudent_Form";
             ((System.ComponentModel.ISupportInitialize)(this.lc_AddStudent)).EndInit();
--- AttendanceManagementSystem/Forms/Students/AddStudent_Form.cs
+++ AttendanceManagementSystem/Forms/Students/AddStudent_Form.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Linq;
@@ -14,124 +13,79 @@ using System.Drawing.Imaging;
 using PdfSharp.Pdf;
 using PdfSharp.Drawing;
 using System.IO;
+using System.Data;
+using AttendanceManagementSystem.Models.Base;
 
 namespace AttendanceManagementSystem.Forms.Students
 {
-	public partial class AddStudent_Form: DevExpress.XtraEditors.XtraForm
-	{
+    public partial class AddStudent_Form : DevExpress.XtraEditors.XtraForm
+    {
         public AddStudent_Form()
-		{
-            InitializeComponent();
-		}
-
-        private Bitmap GenerateQRCode(string content)
         {
-            BarcodeWriter writer = new BarcodeWriter //ZXing class used to generate barcodes and QR codes, configures tto output QR Codes into images
-            {
-                Format = BarcodeFormat.QR_CODE, //Tells ZXign to generate QR codes, BarcodeFormat.QR_CODE is an enum value representing the QR format.
-
-                Options = new ZXing.Common.EncodingOptions //ZXing.Common.EncodingOptions class is used to set the encoding options for the barcode generation.
-                {
-                    Width = 250, //Width of the QR code image
-                    Height = 250, //Height of the QR code image
-                    Margin = 1 //Margin around the QR code
-                }
-            };
-            return writer.Write(content); //Generates the QR code image using the content provided. And returns the generated image as a Bitmap object.
+            InitializeComponent();
         }
 
+        private Student student;
+        private string connectionString = @"Data Source=Data\SEAMS_DB.db;Version=3;";
         private void btn_Generate_Click(object sender, EventArgs e)
         {
-            string qrText = ComposeQRText();
-            Bitmap qrImage = GenerateQRCode(qrText);
-            pe_QRCode.Image = qrImage;
-        }
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txt_FirstName.Text) ||
+                    string.IsNullOrWhiteSpace(txt_LastName.Text) ||
+                    string.IsNullOrWhiteSpace(txt_SchoolStudentId.Text))
+                {
+                    XtraMessageBox.Show("Please fill in all required fields (First Name, Last Name, School ID).");
+                    return;
+                }
 
-        private string ComposeQRText()
-        { 
-            return $"{txt_SchoolStudentId.Text}-{txt_FirstName.Text}-{txt_MiddleName.Text}-{txt_LastName.Text}"; // Assuming these are input fields  
+                student = new Student
+                (
+                    firstName: txt_FirstName.Text,
+                    middleName: txt_MiddleName.Text,
+                    lastName: txt_LastName.Text,
+                    schoolStudentId: txt_SchoolStudentId.Text,
+                    yearLevel: (int)se_YearLevel.Value,
+                    course: cbe_Course.Text,
+                    email: txt_EmailAddress.Text
+                );
+                pe_QRCode.Image = student.QRCode.GenerateQRCode();
+            }
+            catch (Exception ex)
+            {
+                XtraMessageBox.Show($"Error generating QR code: {ex.Message}");
+            }
         }
-
         private void btn_Save_Click(object sender, EventArgs e)
         {
-            string firstName = txt_FirstName.Text;
-            string middleName = txt_MiddleName.Text;
-            string lastName = txt_LastName.Text;
-            string schoolId = txt_SchoolStudentId.Text;
-            string course = cbe_Course.Text;
-            int yearLevel = (int)se_YearLevel.Value;
-            string email = txt_EmailAddress.Text;
-
-            // Compose QR content
-            string qrText = $"{schoolId}|{firstName}|{middleName}|{lastName}|{course}|{yearLevel}|{email}";
-
-            Bitmap qrCodeImage = GenerateQRCode(qrText);
-            pe_QRCode.Image = qrCodeImage; // Show QR code on UI
-
-            // Convert QR image to byte array
-            using (MemoryStream ms = new MemoryStream())
+            if (student == null)
             {
-                qrCodeImage.Save(ms, ImageFormat.Png);
-                byte[] qrBytes = ms.ToArray();
-
-                // Save to SQLite
-                using (SQLiteConnection conn = new SQLiteConnection("Data Source=SEAMS_DB.db"))
-                {
-                    conn.Open();
-                    string sql = @"INSERT INTO Students 
-                (FirstName, MiddleName, LastName, SchoolID, Course, YearLevel, Email, QRImage)
-                VALUES (@FirstName, @MiddleName, @LastName, @SchoolID, @Course, @YearLevel, @Email, @QRImage)";
-
-                    using (SQLiteCommand cmd = new SQLiteCommand(sql, conn))
-                    {
-                        cmd.Parameters.AddWithValue("@FirstName", firstName);
-                        cmd.Parameters.AddWithValue("@MiddleName", middleName);
-                        cmd.Parameters.AddWithValue("@LastName", lastName);
-                        cmd.Parameters.AddWithValue("@SchoolID", schoolId);
-                        cmd.Parameters.AddWithValue("@Course", course);
-                        cmd.Parameters.AddWithValue("@YearLevel", yearLevel);
-                        cmd.Parameters.AddWithValue("@Email", email);
-                        cmd.Parameters.AddWithValue("@QRImage", qrBytes);
-                        cmd.ExecuteNonQuery();
-                    }
-                }
+                XtraMessageBox.Show("Please generate a QR code before saving.");
+                return;
             }
 
-            SaveQRCodeToFile(qrCodeImage, schoolId);
-
-            XtraMessageBox.Show("Student data and QR code saved successfully!");
-        }
-
-        private void SaveQRCodeToFile(Bitmap qrImage, string fileNameBase)
-        {
-            SaveFileDialog sfd = new SaveFileDialog();
-            sfd.Filter = "PNG Image|*.png|PDF Document|*.pdf";
-            sfd.FileName = $"{fileNameBase}_QRCode";
-
-            if (sfd.ShowDialog() == DialogResult.OK)
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
             {
-                if (sfd.FilterIndex == 1) // Save as PNG  
-                {
-                    qrImage.Save(sfd.FileName, ImageFormat.Png);
-                }
-                else if (sfd.FilterIndex == 2) // Save as PDF  
-                {
-                    PdfDocument doc = new PdfDocument();
-                    PdfPage page = doc.AddPage();
-                    XGraphics gfx = XGraphics.FromPdfPage(page);
+                connection.Open();
+                string sql = @"INSERT INTO Student
+                             (FirstName, MiddleName, LastName, SchoolStudentId, Course, YearLevel, Email, QRCode)
+                             VALUES (@FirstName, @MiddleName, @LastName, @SchoolStudentId, @Course, @YearLevel, @Email, @QRCode)";
 
-                    // Fix: Use a MemoryStream to convert Bitmap to XImage  
-                    using (MemoryStream ms = new MemoryStream())
-                    {
-                        qrImage.Save(ms, ImageFormat.Png);
-                        ms.Seek(0, SeekOrigin.Begin);
-                        XImage xImg = XImage.FromStream(ms);
-                        gfx.DrawImage(xImg, 50, 50, 200, 200);
-                    }
-
-                    doc.Save(sfd.FileName);
+                using (SQLiteCommand cmd = new SQLiteCommand(sql, connection))
+                {
+                    cmd.Parameters.AddWithValue("@FirstName", student.FirstName);
+                    cmd.Parameters.AddWithValue("@MiddleName", student.MiddleName);
+                    cmd.Parameters.AddWithValue("@LastName", student.LastName);
+                    cmd.Parameters.AddWithValue("@SchoolStudentId", student.SchoolStudentId);
+                    cmd.Parameters.AddWithValue("@Course", student.Course);
+                    cmd.Parameters.AddWithValue("@YearLevel", student.YearLevel);
+                    cmd.Parameters.AddWithValue("@Email", student.Email);
+                    cmd.Parameters.AddWithValue("@QRCode", student.QRCode.QRCodeValue);
+                    cmd.ExecuteNonQuery();
                 }
             }
+            XtraMessageBox.Show("Student data saved successfully!");
+
         }
     }
 }
