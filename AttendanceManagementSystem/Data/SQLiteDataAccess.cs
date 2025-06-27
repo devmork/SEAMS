25ca106590f87fd7719d90290794be23b0ae4241
devmork <enoymarkden@gmail.com>
2025-06-27 22:26:51 +0800

    feat: add method to load default database connection string

---
+++ AttendanceManagementSystem/Data/SQLiteDataAccess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttendanceManagementSystem.Data
+{
+    public class SQLiteDataAccess
+    {
+        public static string LoadConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        }
+    }
+}
