803829e049f243d2ede74c77672a07a339d7cd88
devmork <enoymarkden@gmail.com>
2025-07-04 02:12:10 +0800

    feat: add Presenters and Views folders with initial interface and class for MVP pattern

---
+++ AttendanceManagementSystem/Views/IAddAttendanceView.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttendanceManagementSystem.Views
+{
+    public interface IAddAttendanceView
+    {
+
+        // MODELS
+        int AttendanceId { get; set; }
+        string AttendanceName { get; set; } 
+        string AttendanceLocation { get; set; }
+        string LogType { get; set; }
+        DateTime Date { get; set; }
+        DateTime StartTime { get; set; }
+        DateTime EndTime { get; set; }
+
+        event EventHandler CreateAttendance;
+
+        void CloseForm();
+        void Cancel();
+        void ClearFields();
+    }
+}
